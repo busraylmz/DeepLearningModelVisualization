7874d16bbe0183e63631be5918e7410cbf4979e5
Büşra Yılmaz <busra_ylmz1993@hotmail.com>
2024-01-14 23:19:29 +0300

    scene updated

---
+++ Assets/Scripts/ClickImage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickImage : MonoBehaviour
+{
+
+    Vector3 goPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goPosition = gameObject.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPositiontoHead()
+    {
+
+
+
+    }
+}
